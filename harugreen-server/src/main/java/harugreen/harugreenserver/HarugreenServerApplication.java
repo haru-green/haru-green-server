b627bcce22d31bc425a69586b0451b37a41105a2
kangCoder <khsofficial1213@gmail.com>
2023-01-27 17:44:03 +0900

    initialize commit

---
+++ harugreen-server/src/main/java/harugreen/harugreenserver/HarugreenServerApplication.java
@@ -0,0 +1,13 @@
+package harugreen.harugreenserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarugreenServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarugreenServerApplication.class, args);
+	}
+
+}
---
+++ harugreen-server/src/test/java/harugreen/harugreenserver/HarugreenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package harugreen.harugreenserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarugreenServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
