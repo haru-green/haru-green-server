6a33170dabe9f4eb2a27adbc067844b5f6018cbb
kangCoder <khsofficial1213@gmail.com>
2023-03-01 22:15:18 +0900

    Fix: /user/get 유저 받아오는 로직 수정

--- harugreen-server/src/main/java/harugreen/harugreenserver/repository/UserRepository.java
+++ harugreen-server/src/main/java/harugreen/harugreenserver/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package harugreen.harugreenserver.repository;
 
 import harugreen.harugreenserver.domain.User;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
-    Optional<List<User>> findAllByRefreshToken(String refreshToken);
     boolean existsUserByEmail(String email);
 }
